2155b3e33cf5c788435207a46553a1c7f475102e
lingabo <37625708+lingabo@users.noreply.github.com>
2021-09-08 09:14:39 +0100

    Update config.php

--- config.php
+++ config.php
@@ -3,8 +3,8 @@
 	define('WEB_NAME', 'Prod');
 	define('WEBSITE_URL', 'http://127.0.0.1:80');
 	define('DB_HOST', 'localhost');
-	define('DB_NAME', 'students');
-	define('DB_USERNAME', 'lingchat');
-	define('DB_PASSWORD', 'french005');
+	define('DB_NAME', 'Kovikis');
+	define('DB_USERNAME', 'root');
+	define('DB_PASSWORD', '');
 
 ?>
